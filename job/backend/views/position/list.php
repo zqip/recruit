01351b30393072c196d1e34cef0e8409408eae70
zqp <953964595@qq.com>
2016-11-10 13:33:02 +0800

    、、

--- job/backend/views/position/list.php
+++ job/backend/views/position/list.php
@@ -6,46 +6,14 @@
 		<meta name="keywords" content="Bootstrap模版,Bootstrap模版下载,Bootstrap教程,Bootstrap中文" />
 		<meta name="description" content="站长素材提供Bootstrap模版,Bootstrap教程,Bootstrap中文翻译等相关Bootstrap插件下载" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-		<!-- basic styles -->
-
 		<link href="assets/css/bootstrap.min.css" rel="stylesheet" />
 		<link rel="stylesheet" href="assets/css/font-awesome.min.css" />
-
-		<!--[if IE 7]>
-		  <link rel="stylesheet" href="assets/css/font-awesome-ie7.min.css" />
-		<![endif]-->
-
-		<!-- page specific plugin styles -->
-
-		<!-- fonts -->
-
 		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" />
-
-		<!-- ace styles -->
-
 		<link rel="stylesheet" href="assets/css/ace.min.css" />
 		<link rel="stylesheet" href="assets/css/ace-rtl.min.css" />
 		<link rel="stylesheet" href="assets/css/ace-skins.min.css" />
-
-		<!--[if lte IE 8]>
-		  <link rel="stylesheet" href="assets/css/ace-ie.min.css" />
-		<![endif]-->
-
-		<!-- inline styles related to this page -->
-
-		<!-- ace settings handler -->
-
 		<script src="assets/js/ace-extra.min.js"></script>
-
-		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-
-		<!--[if lt IE 9]>
-		<script src="assets/js/html5shiv.js"></script>
-		<script src="assets/js/respond.min.js"></script>
-		<![endif]-->
 	</head>
-
 	<body>
 	<?php 
 	use yii\widgets\LinkPager;
@@ -55,7 +23,6 @@
 						<script type="text/javascript">
 							try{ace.settings.check('breadcrumbs' , 'fixed')}catch(e){}
 						</script>
-
 						<ul class="breadcrumb">
 							<li>
 								<i class="icon-home home-icon"></i>
@@ -66,8 +33,7 @@
 								<a href="#">Tables</a>
 							</li>
 							<li class="active">管理中心 </li>
-						</ul><!-- .breadcrumb -->
-
+						</ul>
 						<div class="nav-search" id="nav-search">
 							<form class="form-search">
 								<span class="input-icon">
@@ -75,9 +41,8 @@
 									<i class="icon-search nav-search-icon"></i>
 								</span>
 							</form>
-						</div><!-- #nav-search -->
+						</div>
 					</div>
-
 					<div class="page-content">
 						<div class="page-header">
 							<h1>
@@ -87,12 +52,9 @@
 									职位列表
 								</small>
 							</h1>
-						</div><!-- /.page-header -->
-
+						</div>
 						<div class="row">
 							<div class="col-xs-12">
-								<!-- PAGE CONTENT BEGINS -->
-
 								<div class="row">
 									<div class="col-xs-12">
 										<div class="table-responsive">
@@ -206,38 +168,15 @@
 															</div>
 														</td>
 													</tr>
-
 													<?php } ?>
-														
-												
 												</tbody>
 											</table>
 											<a href="index.php?r=position/list&page=1">首页</a>
 											<a href="index.php?r=position/list&page=<?=$prev ?>">上一页</a>
 											<a href="index.php?r=position/list&page=<?=$next ?>">下一页</a>
 											<a href="index.php?r=position/list&page=<?=$allPage ?>">末页</a>
-										</div><!-- /.table-responsive -->
-									</div><!-- /span -->
-								</div><!-- /row -->
-
-				
-		<!-- basic scripts -->
-
-		<!--[if !IE]> -->
-
-		<script src="assets/js/jquery-2.0.3.min.js"></script>
-
-		<!-- <![endif]-->
-
-		<!--[if IE]>
-<script src="assets/js/jquery-1.10.2.min.js"></script>
-<![endif]-->
-
-		<!--[if !IE]> -->
-
-		<script type="text/javascript">
-			window.jQuery || document.write("<script src='assets/js/jquery-2.0.3.min.js'>"+"<"+"/script>");
-		</script>
-
+										</div>
+									</div>
+								</div>
 </body>
 </html>
