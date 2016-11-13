1c6b36a4fddd37c430b263428bee39ccc4549491
zqp <953964595@qq.com>
2016-11-13 19:17:33 +0800

    # Conflicts: #	job/backend/models/JobPosition.php #	job/backend/runtime/debug/index.data #	job/backend/runtime/logs/app.log #	job/backend/views/layouts/main.php #	lagou/job/backend/runtime/debug/index.data

--- job/backend/models/JobPosition.php
+++ job/backend/models/JobPosition.php
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 <?php
 
 namespace backend\models;
@@ -98,6 +99,109 @@ class JobPosition extends \yii\db\ActiveRecord
             return $arr;
         }
 }
+=======
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "job_admin_privilege".
+ *
+ * @property integer $id
+ * @property string $p_name
+ * @property string $p_controller
+ * @property string $p_action
+ * @property integer $parent_id
+ */
+class JobPosition extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'job_position';
+    }
+
+    /**
+     * @inheritdoc
+     */
+   /** public function rules()
+    {
+        return [
+            [['p_name', 'p_controller', 'p_action', 'parent_id'], 'required'],
+            [['parent_id'], 'integer'],
+            [['p_name', 'p_controller', 'p_action'], 'string', 'max' => 50],
+        ];
+    }
+
+    
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'position_name' => 'Position Name',
+            'parent_id' => 'Parent ID',
+            'is_hot' => 'Is Hot',
+            'is_new' => 'Is New',
+            'is_show' => 'Is Show'
+        ];
+    }
+    public function addData($data){ 
+        $this->setAttributes($data);
+        $this->isNewRecord=true;
+        return $this->save();
+    }
+    public function updateData($data){ 
+        return $this->updateAll($data,['id'=>$data['id']]);
+    }
+    public function getTopData()
+    {
+        return $this->find("id,position_name")->asArray()->all();
+    }
+    public function getwhere($id)
+    {
+        return $this->find()->where(['parent_id'=>$id])->asArray()->all();
+    }
+    public function getone($id)
+    {
+        return $this->find()->where(['id'=>$id])->asArray()->one();
+    }
+    public function getAll()
+    {
+        return $this->find()->asArray()->all();
+    }
+    public function deldate($id){
+        $result=$this->find()->where(['id'=>$id])->one();
+        return $result->delete();
+    }
+    public function getCount()
+    {
+        return $this->find()->count();
+    }
+    public function getLimit($offset,$len)
+    {
+        return $this->find()->offset($offset)->limit($len)->asArray()->all();
+    }
+    //传递一个父级id 返回所有子分类
+      Public function levellist ($cate, $parent_id = 0, $level = 0) {
+         Static $arr = array();
+            foreach ($cate as $k => $v) {
+                if ($v['parent_id'] == $parent_id) {
+                    $v['level'] = $level;
+                    $v['html']  =str_repeat('--',$level*2);
+                    $arr[] = $v;
+                    $arr =self::levellist($cate,$v['id'], $level + 1);
+                }
+            }
+            return $arr;
+        }
+}
+>>>>>>> remotes/origin/cyy
 =======
 <?php
 
